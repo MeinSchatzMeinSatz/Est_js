d143fa679ff9dd6926914320a90af380b9fa89e0
Liebe_Chaeso <d131215@naver.com>
2025-08-21 01:15:17 +0900

    리덕스 인프런 강의 실습

--- 4-ZeeZee_react/0820/redux-toolkit/src/main.jsx
+++ 4-ZeeZee_react/0820/redux-toolkit/src/main.jsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// src/main.jsx
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./store";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
-)
+console.log("Redux Store 초기 상태:", store.getState());
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
